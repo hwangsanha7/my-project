d7e86e3336ace33b862200a1d74974eff3cc7073
hwangsanha7 <hwangsanha7@gmail.com>
2025-06-15 19:50:48 +0900

    ci/cd 1

--- src/setup.py
+++ src/setup.py
@@ -1,40 +1,58 @@
-# setup.py (수정 후)
-from setuptools import setup
+# src/my_robot_pkg/setup.py
+
 import os
 from glob import glob
+from setuptools import setup, find_packages
 
-package_name = 'picar_pkg'
+# ----------------------------------------------------------------
+# ★★★ 1. 패키지 이름 변수를 새로운 이름으로 수정합니다. ★★★
+# ----------------------------------------------------------------
+package_name = 'my_robot_pkg'
 
+# ----------------------------------------------------------------
+# setup() 함수: 패키지에 대한 모든 정보를 colcon에 제공
+# ----------------------------------------------------------------
 setup(
+    # --- 기본 정보 ---
     name=package_name,
-    version='0.0.0',
-    # ★★★★★ 이 부분이 핵심입니다 ★★★★★
-    # 'packages' 대신 'py_modules'를 사용합니다.
-    # 이것은 'picar_pkg' 폴더에서 .py로 끝나는 모든 파일을 모듈로 취급하라는 의미입니다.
-    py_modules=[
-        'talker',
-        'listener'
-    ],
-    # ... (다른 설정들)
+    version='0.0.1',
+    description='My custom robot package for autonomous driving project.',
+    license='Apache License 2.0',
+    
+    # --- 작성자 정보 ---
+    maintainer='Your Name',
+    maintainer_email='your_email@email.com',
+
+    # --- 패키지 및 모듈 설정 ---
+    # 'test' 폴더를 제외한 모든 파이썬 패키지를 자동으로 찾습니다.
+    packages=find_packages(exclude=['test']),
+    
+    # --- 데이터 파일 설정 ---
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
+        
+        # package.xml 파일 포함
         ('share/' + package_name, ['package.xml']),
-        # (고급) launch 파일을 포함시키려면 이 부분을 추가합니다.
-        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py'))
+        
+        # launch 폴더 안의 모든 .launch.py 파일을 자동으로 포함
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
     ],
+
+    # --- 의존성 및 테스트 설정 ---
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='your_name',
-    maintainer_email='your_email@email.com',
-    description='TODO: Package description',
-    license='TODO: License declaration',
     tests_require=['pytest'],
+
+    # --- 실행 파일(노드) 등록 ---
+    #
+    # ★★★ 2. 실행 명령어의 경로도 새로운 패키지 이름으로 수정합니다. ★★★
+    # 형식: '실행명령어 = 패키지이름.파이썬파일:main함수'
+    #
     entry_points={
         'console_scripts': [
-            # '실행명령어 = 파이썬파일:main함수' 형식으로 등록합니다.
-            'talker = talker:main',
-            'listener = listener:main',
+            'talker = my_robot_pkg.talker:main',
+            'listener = my_robot_pkg.listener:main',
         ],
     },
 )
